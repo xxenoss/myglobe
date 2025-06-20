911241228b9a3bcdabc267ee3bd7a6eba6d1355f
Al Ahasun Atique <126971204+xxenoss@users.noreply.github.com>
2025-06-20 13:08:17 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,7 +3,12 @@ import Cors from 'nextjs-cors';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await Cors(req, res, {
-    origin: '*',
+    origin: [
+      'https://my-globe.online',
+      'https://myglobe.vercel.app',
+      'https://myglobe-git-main-xxenoss47-5347s-projects.vercel.app',
+      'https://myglobe-nnjvn9knr-xxenoss47-5347s-projects.vercel.app',
+    ],
     methods: ['GET', 'OPTIONS'],
     optionsSuccessStatus: 200,
   });
