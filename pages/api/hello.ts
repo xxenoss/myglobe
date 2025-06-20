871c97c2d218fce65f8e4d1cfe8afa12a72e968f
Al Ahasun Atique <126971204+xxenoss@users.noreply.github.com>
2025-06-20 12:54:55 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,10 @@ import Cors from 'nextjs-cors';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await Cors(req, res, {
-    origin: '*',
-    methods: ['GET', 'OPTIONS'],
+    origin: 'https://myglobe-6g3hcv0xz-xxenoss47-5347s-projects.vercel.app', // your frontend domain here
+    methods: ['GET', 'POST', 'OPTIONS'],
     optionsSuccessStatus: 200,
   });
 
   res.status(200).json({ message: 'Hello from backend!' });
 }
-
